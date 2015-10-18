fc673339b3f6a3f76e21ed8ed67acadb0d7404e7
Emily <emilyrkarp@gmail.com>
2015-10-18 01:21:15 -0400

    rplcd translate w encode fns; rmvd letter fns from String.prototype

--- morseToggle.js
+++ morseToggle.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var syms = {
+var sym = {
 		dot: '.',
 		dash: '_',
 		space: ' ',
@@ -13,21 +13,11 @@ var syms = {
 		space: -1.5,
 		stop: -2,
 		cut: -1
-	},
-	code = {};
-
-function setCode() {
-	[syms, time].forEach(function(c) {
-		for (var key in c) {
-			code[key].sym = syms.key;
-		}
-	});
-};	setCode();
+	};
 
 Array.prototype.add = function(arr) {
 	for (var i = 0; i < arr.length; i++) {
 		this.push(arr[i]);
-		this.push(1);
 	}
 	return this; 
 };
@@ -38,90 +28,93 @@ Array.prototype.flatten = function() {
 	}, []);
 }
 
-String.prototype.letterSym = function() {
-	switch(this) {
-		case ' ': return [sym.space]; break;
-		case 'a': return [sym.dot,sym.dash]; break;
-		case 'b': return [sym.dash,sym.dot,sym.dot,sym.dot]; break;
-		case 'c': return [sym.dash,sym.dot,sym.dash,sym.dot]; break;
-		case 'd': return [sym.dash,sym.dot,sym.dot]; break;
-		case 'e': return [sym.dot]; break;
-		case 'f': return [sym.dot,sym.dot,sym.dash,sym.dot]; break;
-		case 'g': return [sym.dash,sym.dash,sym.dot]; break;
-		case 'h': return [sym.dot,sym.dot,sym.dot,sym.dot]; break;
-		case 'i': return [sym.dot,sym.dot]; break;
-		case 'j': return [sym.dot,sym.dash,sym.dash,sym.dash]; break;
-		case 'k': return [sym.dash,sym.dot,sym.dash]; break;
-		case 'l': return [sym.dot,sym.dash,sym.dot,sym.dot]; break;
-		case 'm': return [sym.dash,sym.dash]; break;
-		case 'n': return [sym.dash,sym.dot]; break;
-		case 'o': return [sym.dash,sym.dash,sym.dash]; break;
-		case 'p': return [sym.dot,sym.dash,sym.dash,sym.dot]; break;
-		case 'q': return [sym.dash,sym.dash,sym.dot,sym.dash]; break;
-		case 'r': return [sym.dot,sym.dash,sym.dot]; break;
-		case 's': return [sym.dot,sym.dot,sym.dot]; break;
-		case 't': return [sym.dash]; break;
-		case 'u': return [sym.dot,sym.dot,sym.dash]; break;
-		case 'v': return [sym.dot,sym.dot,sym.dot,sym.dash]; break;
-		case 'w': return [sym.dot,sym.dash,sym.dash]; break;
-		case 'x': return [sym.dash,sym.dot,sym.dot,sym.dash]; break;
-		case 'y': return [sym.dash,sym.dot,sym.dash,sym.dash]; break;
-		case 'z': return [sym.dash,sym.dash,sym.dot,sym.dot]; break;
-		default: return [sym.space];
+function letterSym(char) {
+	switch(char) {
+		case ' ': return sym.space; break;
+		case 'a': return sym.dot + sym.dash; break;
+		case 'b': return sym.dash + sym.dot + sym.dot + sym.dot; break;
+		case 'c': return sym.dash + sym.dot + sym.dash + sym.dot; break;
+		case 'd': return sym.dash + sym.dot + sym.dot; break;
+		case 'e': return sym.dot; break;
+		case 'f': return sym.dot + sym.dot + sym.dash + sym.dot; break;
+		case 'g': return sym.dash + sym.dash + sym.dot; break;
+		case 'h': return sym.dot + sym.dot + sym.dot + sym.dot; break;
+		case 'i': return sym.dot + sym.dot; break;
+		case 'j': return sym.dot + sym.dash + sym.dash + sym.dash; break;
+		case 'k': return sym.dash + sym.dot + sym.dash; break;
+		case 'l': return sym.dot + sym.dash + sym.dot + sym.dot; break;
+		case 'm': return sym.dash + sym.dash; break;
+		case 'n': return sym.dash + sym.dot; break;
+		case 'o': return sym.dash + sym.dash + sym.dash; break;
+		case 'p': return sym.dot + sym.dash + sym.dash + sym.dot; break;
+		case 'q': return sym.dash + sym.dash + sym.dot + sym.dash; break;
+		case 'r': return sym.dot + sym.dash + sym.dot; break;
+		case 's': return sym.dot + sym.dot + sym.dot; break;
+		case 't': return sym.dash; break;
+		case 'u': return sym.dot + sym.dot + sym.dash; break;
+		case 'v': return sym.dot + sym.dot + sym.dot + sym.dash; break;
+		case 'w': return sym.dot + sym.dash + sym.dash; break;
+		case 'x': return sym.dash + sym.dot + sym.dot + sym.dash; break;
+		case 'y': return sym.dash + sym.dot + sym.dash + sym.dash; break;
+		case 'z': return sym.dash + sym.dash + sym.dot + sym.dot; break;
+		case '.': return sym.stop;
+		default: return sym.cut;
 	}
 }; 
-String.prototype.letterTime = function() {
-	switch(this) {
-		case ' ': return time.space; break;
-		case 'a': return time.dot + time.dash; break;
-		case 'b': return time.dash + time.dot + time.dot + time.dot; break;
-		case 'c': return time.dash + time.dot + time.dash + time.dot; break;
-		case 'd': return time.dash + time.dot + time.dot; break;
-		case 'e': return time.dot; break;
-		case 'f': return time.dot + time.dot + time.dash + time.dot; break;
-		case 'g': return time.dash + time.dash + time.dot; break;
-		case 'h': return time.dot + time.dot + time.dot + time.dot; break;
-		case 'i': return time.dot + time.dot; break;
-		case 'j': return time.dot + time.dash + time.dash + time.dash; break;
-		case 'k': return time.dash + time.dot + time.dash; break;
-		case 'l': return time.dot + time.dash + time.dot + time.dot; break;
-		case 'm': return time.dash + time.dash; break;
-		case 'n': return time.dash + time.dot; break;
-		case 'o': return time.dash + time.dash + time.dash; break;
-		case 'p': return time.dot + time.dash + time.dash + time.dot; break;
-		case 'q': return time.dash + time.dash + time.dot + time.dash; break;
-		case 'r': return time.dot + time.dash + time.dot; break;
-		case 's': return time.dot + time.dot + time.dot; break;
-		case 't': return time.dash; break;
-		case 'u': return time.dot + time.dot + time.dash; break;
-		case 'v': return time.dot + time.dot + time.dot + time.dash; break;
-		case 'w': return time.dot + time.dash + time.dash; break;
-		case 'x': return time.dash + time.dot + time.dot + time.dash; break;
-		case 'y': return time.dash + time.dot + time.dash + time.dash; break;
-		case 'z': return time.dash + time.dash + time.dot + time.dot; break;
-		default: return time.space;
+function letterTime(char) {
+	switch(char) {
+		case ' ': return [time.space]; break;
+		case 'a': return [time.dot,time.dash]; break;
+		case 'b': return [time.dash,time.dot,time.dot,time.dot]; break;
+		case 'c': return [time.dash,time.dot,time.dash,time.dot]; break;
+		case 'd': return [time.dash,time.dot,time.dot]; break;
+		case 'e': return [time.dot]; break;
+		case 'f': return [time.dot,time.dot,time.dash,time.dot]; break;
+		case 'g': return [time.dash,time.dash,time.dot]; break;
+		case 'h': return [time.dot,time.dot,time.dot,time.dot]; break;
+		case 'i': return [time.dot,time.dot]; break;
+		case 'j': return [time.dot,time.dash,time.dash,time.dash]; break;
+		case 'k': return [time.dash,time.dot,time.dash]; break;
+		case 'l': return [time.dot,time.dash,time.dot,time.dot]; break;
+		case 'm': return [time.dash,time.dash]; break;
+		case 'n': return [time.dash,time.dot]; break;
+		case 'o': return [time.dash,time.dash,time.dash]; break;
+		case 'p': return [time.dot,time.dash,time.dash,time.dot]; break;
+		case 'q': return [time.dash,time.dash,time.dot,time.dash]; break;
+		case 'r': return [time.dot,time.dash,time.dot]; break;
+		case 's': return [time.dot,time.dot,time.dot]; break;
+		case 't': return [time.dash]; break;
+		case 'u': return [time.dot,time.dot,time.dash]; break;
+		case 'v': return [time.dot,time.dot,time.dot,time.dash]; break;
+		case 'w': return [time.dot,time.dash,time.dash]; break;
+		case 'x': return [time.dash,time.dot,time.dot,time.dash]; break;
+		case 'y': return [time.dash,time.dot,time.dash,time.dash]; break;
+		case 'z': return [time.dash,time.dash,time.dot,time.dot]; break;
+		case '.': return [time.stop]; break;
+		default: return [time.cut];
 	}
 }; 
 
-String.prototype.translate = function(time) {
-	if (time) {
-		var convert = String.letterTime,
-			start = [];
-	} else {
-		var convert = String.letterSym,
-			start = '';
-	}
-	
-	return this.reduce(function(result, char) {
-		return result.concat(char.covert());
-	}, start);
+String.prototype.encode = function() {
+	return encode(this, letterSym, '');
+};
+
+String.prototype.encodeTime = function() {
+	return encode(this, letterTime, []);
+};
+
+function encode(str, fn, start) {
+	return str.split('').reduce(function(result, char) {
+		console.log(char, result);
+		return result.concat(fn(char), fn());
+	}, start);	
 }
 
 Number.prototype.transCode = function() {
-	return transcodeChar(this, time, syms);
+	return transcodeChar(this, time, sym);
 };
 String.prototype.transCode = function() {
-	return transcodeChar(this, syms, time);
+	return transcodeChar(this, sym, time);
 };
 
 function transcodeChar(char, code, transCode) {
@@ -136,31 +129,30 @@ function transcodeChar(char, code, transCode) {
 (function($) {
     $.fn.morseToggle = function(userOpts) {
 		
-		var code = translation.time; 
-		
-        var options = $.extend({
+        var opts = $.extend({
 			toggle: "active",
 			string: "SOS", 
 			time: 250
         }, userOpts);
  
-		var morse = options.string.translate(true).flatten();
+		var morse = opts.string.toLowerCase().encodeTime();
 		console.log(morse);
 
 		function blinkText(elem, count) {
-			elem.toggleClass(options.toggle);
+			elem.toggleClass(opts.toggle);
 
 			window.setTimeout(function() {
 
-				morse.length > ++count ? 
-					blinkText(elem, count) :
-					console.log("end");
+				if (morse.length > ++count) { 
+					blinkText(elem, count);
+				}
 
-			}, Math.abs(morse[count] * options.time));
+			}, Math.abs(morse[count] * opts.time));
 		};
 
 		this.each(function() {
 			blinkText($(this), 0);
 		});
+		console.log(opts.string.toLowerCase().encode());
     };
 }( jQuery ));
